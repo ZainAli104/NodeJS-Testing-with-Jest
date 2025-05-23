ed6e0ffb5f0fe4d08addd2e56b970bccc2bdec45
ZainAli <chzainali6363@gmail.com>
2025-05-23 21:28:05 +0500

    first commit

---
+++ src/index.ts
@@ -0,0 +1,51 @@
+import {z} from "zod";
+import express, {RequestHandler} from "express";
+
+export const app = express();
+
+app.use(express.json());
+
+const sumInput = z.object({
+    a: z.number(),
+    b: z.number()
+})
+
+const postSumHandler: RequestHandler = (req, res) => {
+    const parsedResponse = sumInput.safeParse(req.body)
+
+    if (!parsedResponse.success) {
+        res.status(411).json({
+            message: "Incorrect inputs"
+        });
+        return;
+    }
+
+    const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+    res.json({
+        answer
+    });
+};
+
+const getSumHandler: RequestHandler = (req, res) => {
+    const parsedResponse = sumInput.safeParse({
+        a: Number(req.headers["a"]),
+        b: Number(req.headers["b"])
+    })
+
+    if (!parsedResponse.success) {
+        res.status(411).json({
+            message: "Incorrect inputs"
+        });
+        return;
+    }
+
+    const answer = parsedResponse.data.a + parsedResponse.data.b;
+
+    res.json({
+        answer
+    });
+};
+
+app.post("/sum", postSumHandler);
+app.get("/sum", getSumHandler);
