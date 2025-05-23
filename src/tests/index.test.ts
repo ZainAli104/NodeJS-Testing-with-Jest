f836d8140650b80c57a5a5cfd035b9662d791ac7
ZainAli <chzainali6363@gmail.com>
2025-05-23 21:38:38 +0500

    update .gitignore and add test for non-numeric input in sum endpoint

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -30,4 +30,13 @@ describe("POST /sum", () => {
         expect(res.statusCode).toBe(200);
         expect(res.body.answer).toBe(0);
     });
+
+    it("should return 411 when a is not a number", async () => {
+        const res = await request(app).post("/sum").send({
+            a: "a",
+            b: 2
+        });
+        expect(res.statusCode).toBe(411);
+        expect(res.body.message).toBe("Incorrect inputs");
+    })
 });
